385b62ab2a82bc2ad98a3c593ae8eb585dc77250
Igor Ovsiannikov <garek@mail.ru>
2016-08-26 12:38:15 +0300

    update demo

--- src/demo/scripts/components/App.js
+++ src/demo/scripts/components/App.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
-import { showDialog } from '../../../lib/scripts';
-import SimpleDialog from './SimpleDialog';
 
 export default class App extends Component {
-  openDialog() {
-    showDialog(SimpleDialog, {});
-  }
-
   render() {
     return (
       <div>
-        <button onClick={() => this.openDialog()}>Open Dialog</button>
+        <button onClick={this.props.openDialog}>Open Dialog</button>
       </div>
     );
   }
--- src/demo/scripts/main.js
+++ src/demo/scripts/main.js
@@ -5,11 +5,16 @@ import '../styles/general.scss';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './components/App';
-import { DialogHost, dialogStore } from '../../lib/scripts';
+import SimpleDialog from './components/SimpleDialog';
+import { DialogHost, createDialogActions, createDialogistaReducer } from '../../lib/scripts';
+import { createStore } from 'redux';
+const dialogStore = createStore(createDialogistaReducer());
+const { showDialog } = createDialogActions(dialogStore);
 
+const openSimpleDialog = () => showDialog(SimpleDialog, {});
 ReactDOM.render(
   <div className="container">
-    <App />
+    <App openDialog={openSimpleDialog} />
     <DialogHost store={dialogStore} overlayClassName="my-dialog-overlay" className="my-dialog-host"/>
   </div>,
   document.querySelector('#dialogista')
